d4f90c9587cc0660459c429bd1672ac26ef86d26
sangyue1 <sangyue1@lenovo.com>
2017-06-19 19:19:09 +0800

    testgetpost

---
+++ API/src/com/myapp/utils/test/TestGetPost.java
@@ -0,0 +1,204 @@
+package com.myapp.utils.test;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class TestGetPost {
+	/**
+	 * 向指定URL发送GET方法的请求
+	 * 
+	 * @param url
+	 *            发送请求的URL
+	 * @param param
+	 *            请求参数，请求参数应该是name1=value1&name2=value2的形式。
+	 * @return URL所代表远程资源的响应
+	 */
+	public static String sendGet(String url, String param) {
+		String result = "";
+		BufferedReader in = null;
+		try {
+			String urlName = url + "?" + param;
+			URL realUrl = new URL(urlName);
+			// 打开和URL之间的连接
+			URLConnection conn = realUrl.openConnection();
+			// 设置通用的请求属性
+			conn.setRequestProperty("accept", "*/*");
+			conn.setRequestProperty("connection", "Keep-Alive");
+			conn.setRequestProperty("user-agent",
+					"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)");
+			// 建立实际的连接
+			conn.connect();
+			// 获取所有响应头字段
+			Map<String, List<String>> map = conn.getHeaderFields();
+			// 遍历所有的响应头字段
+			for (String key : map.keySet()) {
+				System.out.println(key + "--->" + map.get(key));
+			}
+			// 定义BufferedReader输入流来读取URL的响应
+			in = new BufferedReader(
+					new InputStreamReader(conn.getInputStream()));
+			String line;
+			while ((line = in.readLine()) != null) {
+				result += "/n" + line;
+			}
+		} catch (Exception e) {
+			System.out.println("发送GET请求出现异常！" + e);
+			e.printStackTrace();
+		}
+		// 使用finally块来关闭输入流
+		finally {
+			try {
+				if (in != null) {
+					in.close();
+				}
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * 向指定URL发送POST方法的请求
+	 * 
+	 * @param url
+	 *            发送请求的URL
+	 * @param param
+	 *            请求参数，请求参数应该是name1=value1&name2=value2的形式。
+	 * @return URL所代表远程资源的响应
+	 */
+	public static String sendPost(String url, String param) {
+		PrintWriter out = null;
+		BufferedReader in = null;
+		String result = "";
+		try {
+			URL realUrl = new URL(url);
+			// 打开和URL之间的连接
+			URLConnection conn = realUrl.openConnection();
+			// 设置通用的请求属性
+			conn.setRequestProperty("accept", "*/*");
+			conn.setRequestProperty("connection", "Keep-Alive");
+			conn.setRequestProperty("user-agent",
+					"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)");
+			// 发送POST请求必须设置如下两行
+			conn.setDoOutput(true);
+			conn.setDoInput(true);
+			// 获取URLConnection对象对应的输出流
+			out = new PrintWriter(conn.getOutputStream());
+			// 发送请求参数
+			out.print(param);
+			// flush输出流的缓冲
+			out.flush();
+			// 定义BufferedReader输入流来读取URL的响应
+			in = new BufferedReader(
+					new InputStreamReader(conn.getInputStream()));
+			String line;
+			while ((line = in.readLine()) != null) {
+				result += "/n" + line;
+			}
+		} catch (Exception e) {
+			System.out.println("发送POST请求出现异常！" + e);
+			e.printStackTrace();
+		}
+		// 使用finally块来关闭输出流、输入流
+		finally {
+			try {
+				if (out != null) {
+					out.close();
+				}
+				if (in != null) {
+					in.close();
+				}
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+		return result;
+	}
+	
+	public static void upload(){  
+        List<String> list  = new ArrayList<String>();  //要上传的文件名,如：d:\haha.doc.你要实现自己的业务。我这里就是一个空list.
+        list.add("D:/file/图片test.gif");
+        try {  
+            String BOUNDARY = "---------7d4a6d158c9"; // 定义数据分隔线  
+            URL url = new URL("http://localhost:8080/API/kid/uploadKidImageAction");  
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();  
+            // 发送POST请求必须设置如下两行  
+            conn.setDoOutput(true);  
+            conn.setDoInput(true);  
+            conn.setUseCaches(false);  
+            conn.setRequestMethod("POST");  
+            conn.setRequestProperty("connection", "Keep-Alive");
+            conn.setRequestProperty("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)");  
+            conn.setRequestProperty("Charsert", "UTF-8");   
+            conn.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + BOUNDARY);  
+              
+            OutputStream out = new DataOutputStream(conn.getOutputStream());  
+            byte[] end_data = ("\r\n--" + BOUNDARY + "--\r\n").getBytes();// 定义最后数据分隔线  
+			for (int i = 0; i < list.size(); i++) {
+				String fname = list.get(i);
+				File file = new File(fname);
+				StringBuilder sb = new StringBuilder();
+				sb.append("--");
+				sb.append(BOUNDARY);
+				sb.append("\r\n");
+				sb.append("Content-Disposition: form-data;name=\"file" + i + "\";filename=\"" + file.getName() + "\"\r\n");
+				sb.append("Content-Type:application/octet-stream\r\n\r\n");
+
+				byte[] data = sb.toString().getBytes();
+				out.write(data);
+				DataInputStream in = new DataInputStream(new FileInputStream(file));
+				int bytes = 0;
+				byte[] bufferOut = new byte[1024];
+				while ((bytes = in.read(bufferOut)) != -1) {
+					out.write(bufferOut, 0, bytes);
+				}
+				out.write("\r\n".getBytes()); // 多个文件时，二个文件之间加入这个
+				in.close();
+			}
+            out.write(end_data);  
+            out.flush();    
+            out.close();   
+              
+            // 定义BufferedReader输入流来读取URL的响应  
+            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));  
+            String line = null;  
+            while ((line = reader.readLine()) != null) {  
+                System.out.println(line);  
+            }  
+  
+        } catch (Exception e) {  
+            System.out.println("发送POST请求出现异常！" + e);  
+            e.printStackTrace();  
+        }  
+    }  
+	
+	
+
+	// 提供主方法，测试发送GET请求和POST请求
+	public static void main(String args[]) {
+//		// 发送GET请求
+//		String s = TestGetPost.sendGet("http://localhost:8888/abc/login.jsp",
+//				null);
+//		System.out.println(s);
+//		// 发送POST请求
+//		String s1 = TestGetPost.sendPost("http://localhost:8888/abc/a.jsp",
+//				"user=李刚&pass=abc");
+//		System.out.println(s1);
+		TestGetPost.upload();
+	}
+
+}
